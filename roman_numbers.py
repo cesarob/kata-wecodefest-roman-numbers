31d97435ab934b5ce0fc5ddc89d6b28427496eae
cesar <cesar@nextail.co>
2019-03-02 19:37:11 +0100

    Version that works

--- roman_numbers.py
+++ roman_numbers.py
@@ -1,23 +1,27 @@
-map = {
-    1000: "M",
-    900: "CM",
-    500: "D",
-    400: "CD",
-    100: "C",
-    90: "XC",
-    50: "L",
-    40: "XL",
-    10: "X",
-    9: "IX",
-    5: "V",
-    4: "IV",
-    1: "I"
-}
+MAP = [
+    (1000, "M"),
+    (900, "CM"),
+    (500, "D"),
+    (400, "CD"),
+    (100, "C"),
+    (90, "XC"),
+    (50, "L"),
+    (40, "XL"),
+    (10, "X"),
+    (9, "IX"),
+    (5, "V"),
+    (4, "IV"),
+    (1, "I")
+]
 
 
-def to_roman(number):
-    if number == 0:
+def to_roman(number, map=MAP):
+    if len(map) == 0:
         return ''
-    for key, value in map.items():
-        if number >= key:
-            return value + to_roman(number - key)
+    decimal, roman = map[0]
+    if number > decimal:
+        return roman + to_roman(number - decimal, map)
+    elif number == decimal:
+        return roman
+    else:
+        return to_roman(number, map[1:])
